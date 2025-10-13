29e209a6a4e87d26c726dd93836c96134aa1bdbc
Andy-uno <andgar47@ucm.es>
2025-10-13 17:16:21 +0200

    Añadido de TileMaps e intento de instanciar objetos por tiles

--- ArbolAnimado.cs
+++ ArbolAnimado.cs
@@ -4,21 +4,20 @@ using System;
 public partial class Arbol : StaticBody2D
 {
 	private AnimatedSprite2D anim;
+	private CollisionShape2D collisionShape;
+
+	[Export] public Vector2I CellSize = new Vector2I(64, 64); // Tamaño del tile
 
 	public override void _Ready()
 	{
 		anim = GetNode<AnimatedSprite2D>("Animacion");
+		collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
 
-		// Iniciar animación Idle
 		anim.Play("Idle");
-
-		// Ajustar ZIndex según Y
 		ZIndex = (int)Position.Y;
-	}
 
-	public override void _Process(double delta)
-	{
-		// Solo si el árbol se mueve, sino se puede quitar
-		ZIndex = (int)Position.Y;
+		// Ajustar tamaño del collider automáticamente
+		if (collisionShape.Shape is RectangleShape2D rect)
+			rect.Size = CellSize;
 	}
 }
