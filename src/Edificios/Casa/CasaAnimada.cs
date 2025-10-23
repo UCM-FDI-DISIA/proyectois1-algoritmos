74d0333f493f4cced4ec7b5d49763b241810a609
inespe10 <inespe10@ucm.es>
2025-10-24 00:33:23 +0200

    Cambio el nombre de varios archivos para corregir lo de los recursos. La mina de Oro no estaba actualizada, asi que cogi la de la version "mejora colision mina de oro"

--- src/Edificios/Casa/CasaAnimada.cs
+++ src/Edificios/Casa/CasaAnimada.cs
@@ -3,4 +3,13 @@ using System;
 
 public partial class CasaAnimada : Node2D
 {
+	public static int numCasas = 0;
+	
+	// Esto se ejecuta una sóla vez.
+	public override void _Ready(){
+		numCasas++;
+		
+		// Para comprobar que todo funciona bien, se 
+		GD.Print("Hay " + numCasas + " casas.");
+	}
 }
