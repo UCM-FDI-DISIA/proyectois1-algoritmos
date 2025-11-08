16c4f9e271be31488c5455bbd61d2c7c262769b3
cvillo01 <cvillo01@ucm.es>
2025-11-08 17:15:59 +0100

    Codigo del main cambiado a gd

---
+++ src/Main.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# =====================
+# VARIABLES Y NODOS
+# =====================
+var capa_suelo : TileMapLayer
+var capa_objetos : Node2D
+
+# =====================
+# INICIALIZACIÓN
+# =====================
+func _ready() -> void:
+	# Obtener referencias a los nodos
+	# capa_suelo = $Mapa/Suelo_0
+	# capa_objetos = $Mapa/Objetos
+	pass
